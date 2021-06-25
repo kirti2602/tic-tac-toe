edfb79350eb4e3b4847e59944345405ebe993b1d
Reetik Rajan <44093575+r4reetik@users.noreply.github.com>
2021-06-25 20:06:10 +0530

    enhancements in code
    + fix bLine
    + complete structure revamped
    + more subtle file names

--- toe.js
+++ toe.js
@@ -1,96 +1,131 @@
-let cells = document.querySelectorAll('.row > div');
-
-// document.getElementById('result').style.visibility = 'hidden'; 
-
+let cells = document.querySelectorAll("td");
 
 for (let i = 0; i < cells.length; i++) {
-    cells[i].addEventListener('click', clickHandler);
+    cells[i].addEventListener("click", clickHandler);
 }
 
-let xTurn = true, drawDecider = false;
+let xTurn = true,
+    drawDecider = false;
 
-function clickHandler(){
-    if(event.target.textContent){
-    }
-
-    else if(xTurn){
-        event.target.textContent = 'X';
-    }
-
-    else{
-        event.target.textContent = 'O';
+function clickHandler(event) {
+    if (event.target.textContent) {
+    } else if (xTurn) {
+        event.target.textContent = "X";
+    } else {
+        event.target.textContent = "O";
     }
     xTurn = !xTurn;
 
     //logic for winner declaration
-    if(cells[0].textContent == 'X' && cells[1].textContent == 'X' && cells[2].textContent == 'X' ){
+    if (
+        cells[0].textContent == "X" &&
+        cells[1].textContent == "X" &&
+        cells[2].textContent == "X"
+    ) {
         xWins();
-    }
-
-    else if(cells[0].textContent == 'O' && cells[1].textContent == 'O' && cells[2].textContent == 'O' ){
+    } else if (
+        cells[0].textContent == "O" &&
+        cells[1].textContent == "O" &&
+        cells[2].textContent == "O"
+    ) {
         oWins();
-    }
-
-    else if(cells[3].textContent == 'X' && cells[4].textContent == 'X' && cells[5].textContent == 'X' ){
+    } else if (
+        cells[3].textContent == "X" &&
+        cells[4].textContent == "X" &&
+        cells[5].textContent == "X"
+    ) {
         xWins();
-    }
-
-    else if(cells[3].textContent == 'O' && cells[4].textContent == 'O' && cells[5].textContent == 'O' ){
+    } else if (
+        cells[3].textContent == "O" &&
+        cells[4].textContent == "O" &&
+        cells[5].textContent == "O"
+    ) {
         oWins();
-    }
-
-    else if(cells[6].textContent == 'X' && cells[7].textContent == 'X' && cells[8].textContent == 'X' ){
+    } else if (
+        cells[6].textContent == "X" &&
+        cells[7].textContent == "X" &&
+        cells[8].textContent == "X"
+    ) {
         xWins();
-    }
-
-    else if(cells[6].textContent == 'O' && cells[7].textContent == 'O' && cells[8].textContent == 'O' ){
+    } else if (
+        cells[6].textContent == "O" &&
+        cells[7].textContent == "O" &&
+        cells[8].textContent == "O"
+    ) {
         oWins();
-    }
-
-    else if(cells[0].textContent == 'X' && cells[3].textContent == 'X' && cells[6].textContent == 'X' ){
+    } else if (
+        cells[0].textContent == "X" &&
+        cells[3].textContent == "X" &&
+        cells[6].textContent == "X"
+    ) {
         xWins();
-    }
-
-    else if(cells[0].textContent == 'O' && cells[3].textContent == 'O' && cells[6].textContent == 'O' ){
-        alert('O wins');
+    } else if (
+        cells[0].textContent == "O" &&
+        cells[3].textContent == "O" &&
+        cells[6].textContent == "O"
+    ) {
+        alert("O wins");
         window.location.reload();
-    }
-
-    else if(cells[1].textContent == 'X' && cells[4].textContent == 'X' && cells[7].textContent == 'X' ){
+    } else if (
+        cells[1].textContent == "X" &&
+        cells[4].textContent == "X" &&
+        cells[7].textContent == "X"
+    ) {
         xWins();
-    }
-
-    else if(cells[1].textContent == 'O' && cells[4].textContent == 'O' && cells[7].textContent == 'O' ){
+    } else if (
+        cells[1].textContent == "O" &&
+        cells[4].textContent == "O" &&
+        cells[7].textContent == "O"
+    ) {
         oWins();
-    }
-
-    else if(cells[2].textContent == 'X' && cells[5].textContent == 'X' && cells[8].textContent == 'X' ){
+    } else if (
+        cells[2].textContent == "X" &&
+        cells[5].textContent == "X" &&
+        cells[8].textContent == "X"
+    ) {
         xWins();
-    }
-
-    else if(cells[2].textContent == 'O' && cells[5].textContent == 'O' && cells[8].textContent == 'O' ){
+    } else if (
+        cells[2].textContent == "O" &&
+        cells[5].textContent == "O" &&
+        cells[8].textContent == "O"
+    ) {
         oWins();
-    }
-
-    else if(cells[0].textContent == 'X' && cells[4].textContent == 'X' && cells[8].textContent == 'X' ){
+    } else if (
+        cells[0].textContent == "X" &&
+        cells[4].textContent == "X" &&
+        cells[8].textContent == "X"
+    ) {
         xWins();
-    }
-
-    else if(cells[0].textContent == 'O' && cells[4].textContent == 'O' && cells[8].textContent == 'O' ){
+    } else if (
+        cells[0].textContent == "O" &&
+        cells[4].textContent == "O" &&
+        cells[8].textContent == "O"
+    ) {
         oWins();
-    }
-
-    else if(cells[2].textContent == 'X' && cells[4].textContent == 'X' && cells[6].textContent == 'X' ){
+    } else if (
+        cells[2].textContent == "X" &&
+        cells[4].textContent == "X" &&
+        cells[6].textContent == "X"
+    ) {
         xWins();
-    }
-
-    else if(cells[0].textContent == 'O' && cells[4].textContent == 'O' && cells[8].textContent == 'O' ){
+    } else if (
+        cells[0].textContent == "O" &&
+        cells[4].textContent == "O" &&
+        cells[8].textContent == "O"
+    ) {
         oWins();
-    }
-
-    else{
-        if(cells[0].textContent != '' && cells[1].textContent != '' && cells[2].textContent != '' && cells[3].textContent != '' && cells[4].textContent != '' && 
-        cells[5].textContent != '' && cells[6].textContent != '' && cells[7].textContent != '' && cells[8].textContent != '' ){
+    } else {
+        if (
+            cells[0].textContent != "" &&
+            cells[1].textContent != "" &&
+            cells[2].textContent != "" &&
+            cells[3].textContent != "" &&
+            cells[4].textContent != "" &&
+            cells[5].textContent != "" &&
+            cells[6].textContent != "" &&
+            cells[7].textContent != "" &&
+            cells[8].textContent != ""
+        ) {
             drawDecider = true;
             announceDraw(drawDecider);
         }
@@ -98,27 +133,26 @@ function clickHandler(){
 }
 
 //function to handle wins by X
-function xWins(){
-    document.getElementById('result').textContent = 'Winner: Player 1';
+function xWins() {
+    document.getElementsByClassName("result")[0].textContent = "Winner: Player 1";
     setTimeout(announceResult, 2500);
 }
 
 //Function to handle wins by O
-function oWins(){
-    document.getElementById('result').textContent = 'Winner: Player 2';
+function oWins() {
+    document.getElementsByClassName("result")[0].textContent = "Winner: Player 2";
     setTimeout(announceResult, 2500);
 }
-
 
 //function to handle draw
-function announceDraw(drawDecider){
-    if(drawDecider){
-        document.getElementById('result').textContent = 'Draw❌';
-    setTimeout(announceResult, 2500);
+function announceDraw(drawDecider) {
+    if (drawDecider) {
+        document.getElementsByClassName("result")[0].textContent = "Draw❌";
+        setTimeout(announceResult, 2500);
     }
 }
-    
+
 //function to reset game
-function announceResult(){
+function announceResult() {
     window.location.reload();
 }
